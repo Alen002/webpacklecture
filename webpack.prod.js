edf0610a50e2c62a382f2fd618ac4696cc8327f3
Alen <alechkr@gmail.com>
2020-05-07 11:44:16 +0200

    created dev and dev config files

---
+++ webpack.prod.js
@@ -0,0 +1,29 @@
+onst path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "production",
+    /* devtool: "none", */
+    entry: "./src/index.js",
+    output: {
+        filename: "main.[contentHash].js",   //Hash included in the filename
+        path: path.resolve(__dirname, "dist")
+    },
+
+    module: {
+        rules: [
+          {
+            test: /\.scss$/,
+            use: [
+              "style-loader", //Thirs - Inject styles into DOM
+              "css-loader", // Second - Turns css into js
+              "sass-loader" // First - Turns sass into css
+            ]
+          },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    })],
+};
